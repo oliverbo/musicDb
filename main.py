be88a134a4fa9bad394744459271c15fba460524
Oliver Bouchard <oliver@enobo.com>
2014-04-06 15:14:21 -0400

    Added authentication to admin page and removed redirection

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import os
 import urllib
 import logging
 
+from google.appengine.api import users
+
 import webapp2
+import jinja2
+
+logger = logging.getLogger("main")
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + '/html'),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True
+)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -10,10 +20,18 @@ class MainPage(webapp2.RequestHandler):
         
 class AdminPage(webapp2.RequestHandler):
     def get(self):
-        self.redirect('html/admin.html', True)
-    
+        user = users.get_current_user()
+                
+        if (user):
+            logger.info('Authenticated user: %s', user.nickname())
+            template = JINJA_ENVIRONMENT.get_template('admin.html')
+            template_values = { "userName" : user.nickname()}
+            self.response.write(template.render(template_values))
+        else:
+            logger.info('No user authenticated')
+            self.redirect(users.create_login_url('/admin2'))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/admin', AdminPage)
+    ('/admin2', AdminPage)
 ], debug=True)
