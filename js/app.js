36760e8631d881965c01b7e9c9280610e92d470c
Oliver Bouchard <oliver@enobo.com>
2014-05-03 22:12:55 -0400

    added Angular routing

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 var musicDbApp = angular.module('musicDbApp', 
-	[ 'pageModule', 'musicDbServices', 'musicDbControllers', 'ui.bootstrap']);
+	['pageModule', 'musicDbServices', 'ngRoute', 'musicDbControllers', 'ui.bootstrap']);
+	
+musicDbApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/artists', {
+				templateUrl : 'html/partials/artists.html',
+				controller : 'ArtistController'
+			}).
+			when('/venues', {
+				templateUrl : 'html/partials/venues.html',
+				controller : 'VenueController'
+			}).
+			otherwise({
+				redirectTo : '/venues'
+			});		
+		}
+	]);
--- main.py
+++ main.py
@@ -15,12 +15,17 @@ apptools.web_root = os.path.dirname(__file__) + '/html'
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		page_handler.static_page(self, "index.html")
+		
+class VenuePage(webapp2.RequestHandler):
+	def get(self):
+		page_handler.static_page(self, "venue.html")
+
             
 class AdminPage(webapp2.RequestHandler):
 	def get(self):
 		page_handler.static_page(self, "admin.html", auth_mode = page_handler.AUTH_ADMIN)        
     
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/admin', AdminPage)
+    ('/admin', AdminPage),
+    ('/.*', MainPage)
 ], debug=True)
