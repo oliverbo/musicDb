d75303be3738f765be49668e79843c639f9be4de
Oliver Bouchard <oliver@enobo.com>
2014-06-11 20:59:52 -0400

    Added validation framework

--- musicdb/tools.py
+++ musicdb/tools.py
@@ -5,3 +5,6 @@ import json
 # Converts a NDB object to JSON
 def ndb_to_json(obj):    
 	return json.dumps(obj, default = lambda o: o.to_dict())
+	
+def to_json(obj):    
+	return json.dumps(obj, default = lambda o: o.__dict__)
